ac5e3e4e704c8a91b9eae4316c57312e7b7b8871
Jedle <skely@volny.cz>
2020-03-30 14:46:48 +0200

    added global coordinates from BVH file (BVwhacker modified for python3 - limited function)

---
+++ bvh2glo_simple.py
@@ -0,0 +1,19 @@
+from BVwHacker import bvh
+from lib import BVH
+import numpy as np
+
+def calculate(_in_file):
+    trajectory = BVH.load_trajectory(_in_file)
+    header = BVH.load_raw_header(_in_file)
+    tree_structure = BVH.get_tree_structure_joint_list(header)
+    joints, channels, offsets, _, _ = tree_structure
+    skeleton = bvh.Skeleton(_in_file, 1)
+    total_length = np.size(trajectory, 0)
+
+    retval = np.zeros((total_length, len(joints), 3))
+    for i in range(total_length):
+        skeleton.updateFrame(i)
+        for c, joint in enumerate(joints):
+            j = skeleton.getJoint(joint)
+            retval[i, c, :] = j.worldpos[0], j.worldpos[1], j.worldpos[2]
+    return joints, retval
---
+++ simple_run.py
@@ -0,0 +1,55 @@
+import bvh2glo_simple
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
+def my_colored_plot(_joint):
+    if any(n in _joint for n in ['Right']):
+        m_color = 'r'
+    elif any(n in _joint for n in ['Left']):
+        m_color = 'b'
+    else:
+        m_color = 'g'
+
+    if any(n in _joint for n in ['1', '2', '3']):  # prsty
+        m_shape = '+'
+    elif any(n in _joint for n in ['Shoulder']):
+        m_shape = 's'
+    elif any(n in _joint for n in ['ForeArm']):
+        m_shape = 'p'
+    elif any(n in _joint for n in ['Arm']):
+        m_shape = 'v'
+    else:
+        m_shape = '*'
+
+    return m_color, m_shape
+
+
+if __name__ == '__main__':
+    """
+    Demonstrator: how to use bvh2glo_simple.py
+    """
+
+    # BVH file
+    BVH_file = '/home/jedle/data/Sign-Language/_source_clean/bvh/16_05_29_c_FR.bvh'
+    # frame_number
+    frame_number = 1000
+    # size of axes in graph
+    unisize = 50
+    
+    joints, results = bvh2glo_simple.calculate(BVH_file)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d', title='frame: {}'.format(frame_number))
+    for i, joint in enumerate(joints):
+        m_color, m_shape = my_colored_plot(joint)
+        ax.scatter(results[frame_number, i, 0], results[frame_number, i, 1], results[frame_number, i, 2], label=joint, color=m_color, marker=m_shape)
+
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+    ax.set_xlim3d(-unisize, unisize)
+    ax.set_ylim3d(-unisize, unisize)
+    ax.set_zlim3d(-unisize, unisize)
+    ax.legend()
+    plt.show()
+    # break
