88d564e68ebbf52d1b8ecc49df1f9e4d9f2ed1b9
Jedle <skely@volny.cz>
2020-04-08 08:59:57 +0200

    cleaning and tests update

---
+++ tests/LSTM/LSTM_test_stacked.py
@@ -0,0 +1,21 @@
+import os
+import datetime
+from tests.LSTM import LSTM_train
+
+
+if __name__ == '__main__':
+    ref_time = datetime.datetime.now()
+    set_epochs = [3]
+    set_kernels = [8]
+    experiment = 'test_glo_v4'
+    source_dir = '/home/jedle/data/Sign-Language/_source_clean/testing'
+    data_dir = os.path.join(source_dir, experiment)
+    prepared_data_file = os.path.join(source_dir, 'prepared_data_glo_30-30ns.npz')
+    logfile = os.path.join(data_dir, 'losses.txt')
+    NN_type = 'stacked'
+
+    for k in set_kernels:
+        for e in set_epochs:
+            history = getattr(LSTM_train, NN_type)(prepared_data_file, data_dir, k, e)
+            akt_result = [{'ref_time': ref_time, 'NN_version': NN_type, 'kernels' : k, 'epochs' : e, 'loss' : history.history['loss'][-1], 'val_loss' : history.history['val_loss'][-1]}]
+            LSTM_train.make_log(akt_result, logfile)
--- tests/LSTM/LSTM_train.py
+++ tests/LSTM/LSTM_train.py
@@ -24,7 +24,7 @@ import fastdtw
 import json
 
 
-def train_LSTM_bidirectional(_datafile, _output_dir, _LSTM_kernels, _epochs):
+def bidirectional(_datafile, _output_dir, _LSTM_kernels, _epochs):
     """
     Trains LSTM bidirectional network with given parameters.
     :param _datafile: datafile prepared by LSTM_dataprep
@@ -60,7 +60,7 @@ def train_LSTM_bidirectional(_datafile, _output_dir, _LSTM_kernels, _epochs):
     return history
 
 
-def train_LSTM(_datafile, _output_dir, _LSTM_kernels, _epochs):
+def simple_autoencoder(_datafile, _output_dir, _LSTM_kernels, _epochs):
     """
     Trains LSTM bidirectional network with given parameters.
     :param _datafile: datafile prepared by LSTM_dataprep
@@ -96,6 +96,47 @@ def train_LSTM(_datafile, _output_dir, _LSTM_kernels, _epochs):
     return history
 
 
+def stacked(_datafile, _output_dir, _LSTM_kernels, _epochs):
+    """
+    Trains LSTM bidirectional network with given parameters.
+    :param _datafile: datafile prepared by LSTM_dataprep
+    :param _output_dir: model and log is saved there
+    :param _LSTM_kernels:
+    :param _epochs:
+    :return:
+    """
+    layer2 = int(_LSTM_kernels/2)
+    layer3 = int(layer2/2)
+    model_code = 'model_LSTM_AE_ker{}_ep{}'.format(_LSTM_kernels, _epochs)
+    model_file = os.path.join(_output_dir, model_code + '.h5')
+    log_file = os.path.join(_output_dir, model_code + '.txt')
+
+    data = np.load(_datafile)
+    train_X = data['train_X']
+    train_Y = data['train_Y']
+
+    batch, time, features = np.shape(train_X)
+
+    model = Sequential()
+    model.add(LSTM(_LSTM_kernels, activation='relu', input_shape=(time, features), return_sequences=True))
+    model.add(LSTM(layer2, activation='relu', return_sequences=True))
+    model.add(LSTM(layer3, activation='relu', return_sequences=True))
+    # model.add(RepeatVector(time))
+    # model.add(LSTM(layer2, activation='relu', return_sequences=True))
+    # model.add(LSTM(_LSTM_kernels, activation='relu', return_sequences=True))
+    model.add(TimeDistributed(Dense(features)))
+    # model.compile(optimizer='adam', loss='mse')
+    model.compile(optimizer='SGD', loss='mse')
+    print(model.summary())
+    history = model.fit(train_X, train_Y, epochs=_epochs, validation_split=0.1, shuffle=True, verbose=2, batch_size=100)
+    model.save(model_file)
+
+    with open(log_file, 'w') as f:
+        json.dump(history.history, f)
+
+    return history
+
+
 def make_log(_log_entry, _log_file):
     if os.path.isfile(_log_file):
         with open(_log_file, 'r') as f:
@@ -104,27 +145,27 @@ def make_log(_log_entry, _log_file):
     else:
         new_res = _log_entry
 
-    with open(logfile, 'w') as f:
+    with open(_log_file, 'w') as f:
         json.dump(new_res, f)
 
 
-if __name__ == '__main__':
-    set_epochs = [1000, 2000]
-    set_kernels = [200, 300, 400]
-    experiment = 'test_glo_v2'
-    source_dir = '/home/jedle/data/Sign-Language/_source_clean/testing'
-    data_dir = os.path.join('/home/jedle/data/Sign-Language/_source_clean/testing', experiment)
-    prepared_data_file = os.path.join(source_dir, 'prepared_data_glo_30-30ns.npz')
-    logfile = os.path.join(data_dir, 'losses.txt')
-    NN_type = 'train_LSTM'
-    NN_function = globals()[NN_type]
-
-    for k in set_kernels:
-        for e in set_epochs:
-
-            history = NN_function(prepared_data_file, data_dir, k, e)
-            akt_result = [{'NN_version': NN_type, 'kernels' : k, 'epochs' : e, 'loss' : history.history['loss'][-1], 'val_loss' : history.history['val_loss'][-1]}]
-            make_log(akt_result, logfile)
+# if __name__ == '__main__':
+#     set_epochs = [3]
+#     set_kernels = [8]
+#     experiment = 'test_glo_v2'
+#     source_dir = '/home/jedle/data/Sign-Language/_source_clean/testing'
+#     data_dir = os.path.join('/home/jedle/data/Sign-Language/_source_clean/testing', experiment)
+#     prepared_data_file = os.path.join(source_dir, 'prepared_data_glo_30-30ns.npz')
+#     logfile = os.path.join(data_dir, 'losses.txt')
+#     NN_type = 'train_LSTM_stacked'
+#     NN_function = globals()[NN_type]
+#
+#     for k in set_kernels:
+#         for e in set_epochs:
+#
+#             history = NN_function(prepared_data_file, data_dir, k, e)
+#             akt_result = [{'NN_version': NN_type, 'kernels' : k, 'epochs' : e, 'loss' : history.history['loss'][-1], 'val_loss' : history.history['val_loss'][-1]}]
+#             make_log(akt_result, logfile)
             # if os.path.isfile(logfile):
             #     with open(logfile, 'r') as f:
             #         old_res = json.load(f)
