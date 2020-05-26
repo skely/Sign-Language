db6ce3c4968d69506bffa244fb38c67a45084a96
Jedle <jedlicka@kky.zcu.cz>
2020-05-26 14:50:14 +0200

    modifikace moje.

--- tra_corelation_test.py
+++ tra_corelation_test.py
@@ -1,3 +1,4 @@
+import os
 from lib import bvh2glo_simple
 import numpy as np
 from lib import SL_dict
@@ -9,10 +10,18 @@ import glob
 
 if __name__ == "__main__":
     done_files = 0
-    for filepath in glob.iglob('C:/Users/User/PRJ4/data_bvh/*.bvh'): #iterování přes jednotlivé soubory
-        BVH_file = filepath
 
-        dictionary_file = 'C:/Users/User/PRJ4/data/ultimate_dictionary2.txt'
+    # file_list = glob.iglob('C:/Users/User/PRJ4/data_bvh/*.bvh')
+    path = '/home/jedle/data/Sign-Language/_source_clean/bvh'
+    file_list = os.listdir(path)
+    file_list = [f for f in file_list if ('17' in f or '16' in f)]
+    # dictionary_file = 'C:/Users/User/PRJ4/data/ultimate_dictionary2.txt'
+    dictionary_file = '/home/jedle/Projects/Sign-Language/data/ultimate_dictionary2.txt'
+
+    for filepath in file_list: #iterování přes jednotlivé soubory
+        if '16_10_25_a_R' in filepath or '17_01_15_b_FR' in filepath or '17_03_25_b_R' in filepath:
+            continue
+        BVH_file = os.path.join(path, filepath)
 
         #načtení BVH souboru a přepočítání angulárních dat na trajektorii v globáních souřadnicích
         #BVH_infile = '16_05_20_a_R.bvh'
@@ -42,6 +51,7 @@ if __name__ == "__main__":
 
         Dist_sorted = [] #přeuspořádání DISTS, index řádku = část těla, obsahuje všechny polohy dané části těla ve snímcích, nutno pro použití ve funkci np.corrcoef()
         numberof = 0
+        print(filepath)
         while numberof < len(DISTS[1]):
             Dist_sorted.append([])
             for i in range(len(DISTS)):
@@ -69,17 +79,17 @@ if __name__ == "__main__":
             maxx.append(corr_coefs.pop(corr_coefs.index(max(corr_coefs))))
             #avg_dists.append(np.mean(Dist_sorted[indexes[i]]))
 
-        file = open("results.txt","a") #append mode do souboru s informacemi
-        file.write('\n********************************************************\n')
-        file.write(BVH_file)
-        file.write('\nBiggest correlation: '+str(joints[indexes[0]])+', val: '+str(maxx[0])+', avg trav. dist: '+str(avg_dist_singly[indexes[0]]))
-        file.write('\nSecond b. correlation: '+str(joints[indexes[1]])+', val: '+str(maxx[1])+', avg trav. dist: '+str(avg_dist_singly[indexes[1]]))
-        file.write('\nThird b. correlation: '+str(joints[indexes[2]])+', val: '+str(maxx[2])+', avg trav. dist: '+str(avg_dist_singly[indexes[2]])+'\nAvg trav. dist all: '+str(avg_dist_all))
-        file.close()
+        # file = open("results.txt","a") #append mode do souboru s informacemi
+        # file.write('\n********************************************************\n')
+        # file.write(BVH_file)
+        # file.write('\nBiggest correlation: '+str(joints[indexes[0]])+', val: '+str(maxx[0])+', avg trav. dist: '+str(avg_dist_singly[indexes[0]]))
+        # file.write('\nSecond b. correlation: '+str(joints[indexes[1]])+', val: '+str(maxx[1])+', avg trav. dist: '+str(avg_dist_singly[indexes[1]]))
+        # file.write('\nThird b. correlation: '+str(joints[indexes[2]])+', val: '+str(maxx[2])+', avg trav. dist: '+str(avg_dist_singly[indexes[2]])+'\nAvg trav. dist all: '+str(avg_dist_all))
+        # file.close()
         done_files +=1
         print(done_files)
-        #print('Biggest correlation with:',joints[indexes[0]],', value:',maxx[0],', avg travelled dist:',avg_dist_singly[indexes[0]])
-        #print('Second biggest correlation with:',joints[indexes[1]],', value:',maxx[1],', avg travelled dist:',avg_dist_singly[indexes[1]])
-        #print('Third biggest correlation with:',joints[indexes[2]],', value:',maxx[2],', avg travelled dist:',avg_dist_singly[indexes[2]],'\nAvg travelled dist for all:',avg_dist_all)
-        #plt.scatter(Dist_sorted[indexes[0]], ranges.values())
-        #plt.show()
+        print('Biggest correlation with:',joints[indexes[0]],', value:',maxx[0],', avg travelled dist:',avg_dist_singly[indexes[0]])
+        print('Second biggest correlation with:',joints[indexes[1]],', value:',maxx[1],', avg travelled dist:',avg_dist_singly[indexes[1]])
+        print('Third biggest correlation with:',joints[indexes[2]],', value:',maxx[2],', avg travelled dist:',avg_dist_singly[indexes[2]],'\nAvg travelled dist for all:',avg_dist_all)
+        # plt.scatter(Dist_sorted[indexes[0]], ranges.values())
+    # plt.show()
