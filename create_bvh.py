df8264822498aecd0371b66f605cb9ace2a813b3
Jedle <jedlicka@kky.zcu.cz>
2020-12-03 14:47:32 +0100

    zeroing position of root enabled

--- create_bvh.py
+++ create_bvh.py
@@ -10,4 +10,4 @@ new_data_file = os.path.join(source_dir, 'prepared_data_30-30_aug10times2.npz')
 loaded_data = np.load(new_data_file)
 data_focus = loaded_data['train_X'][0]
 
-BVH.generate_BVH(data_focus, 'out_BVH_func.bvh', template_bvh_file, channels='rotation')
+BVH.generate_BVH(data_focus, 'nonzero_base.bvh', template_bvh_file, channels='rotation', zero_shift=False)
